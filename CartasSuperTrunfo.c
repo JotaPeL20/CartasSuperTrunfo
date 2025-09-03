7ac148949432d6952d467007a61c2efd72dccd23
João Pedro de Oliveira <lorym000@gmail.com>
2025-09-03 15:02:29 +0000

    Finalizando o desafio Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@ int main(){
   char Estado1;
   char Codigo_da_carta1[4];
   char Nome_da_cidade1[50];
-  int Populacao1, Pontos_Turisticos1;
+  int  Populacao1, Pontos_Turisticos1;
   float Area1, PIB1;
   float PIB_Per_Capita1, Densidade_Populacional1;
 
@@ -15,40 +15,41 @@ int main(){
   char Estado2;
   char Codigo_da_carta2[4];
   char Nome_da_cidade2[50];
-  int Populacao2, Pontos_Turisticos2;
+  int Pontos_Turisticos2, Populacao2;
   float Area2, PIB2;
   float PIB_Per_Capita2, Densidade_Populacional2;
     
     
   // Cabeçalho do jogo
+
   printf("SUPER TRUNFO DE PAÍSES - CADASTRO DE CARTAS\n\n");
 
   // Leitura de dados da primeira carta
   printf("Registre a sua carta 1\n\n");
 
-  printf("Digite o Estado (A-H): ");
+  printf("Digite o Estado (A-H): ");//O jogador irá digitar a letra correspondente ao Estado
   scanf("%c", &Estado1);
 
-  printf("Digite o Código da Carta(ex: A01): ");
+  printf("Digite o Código da Carta(ex: A01): ");//O jogador ira digitar o código da carta
   scanf("%3s", Codigo_da_carta1);
 
-  printf("Digite o Nome da Cidade: ");
+  printf("Digite o Nome da Cidade: ");//O jogador ira digitar o nome da cidade presente na carta
   scanf("%s", Nome_da_cidade1);
 
-  printf("Digite a População: ");
+  printf("Digite a População: ");//O jogador ira digitar a populaçao da cidade
   scanf("%d", &Populacao1);
 
-  printf("Digite a Área da Cidade (km²): ");
+  printf("Digite a Área da Cidade (km²): ");//O jogador ira digitar a área da cidade 
   scanf("%f", &Area1);
 
-  printf("Digite o PIB (bilhões de reais): ");
+  printf("Digite o PIB (bilhões de reais): ");//O jogador ira digitar o PIB da cidade
   scanf("%f", &PIB1);
 
-  printf("Digite o número de Pontos Turísticos: ");
+  printf("Digite o número de Pontos Turísticos: ");//O jogador ira digitar o numero de pontos turísticos da cidade
   scanf("%d", &Pontos_Turisticos1);
 
   // Cálculos após a leitura dos dados
-  PIB_Per_Capita1 = (float) PIB1 / Populacao1;
+  PIB_Per_Capita1 = (float) (PIB1 * 1000000000) / Populacao1;//Será convertido bilhões para unidades
   Densidade_Populacional1 = (float) Populacao1 / Area1;
 
   printf("\n\n");
@@ -57,31 +58,31 @@ int main(){
 
   printf("Registre a sua carta 2\n\n");
 
-  printf("Digite o estado (A-H): ");
+  printf("Digite o estado (A-H): ");//O jogador irá digitar a letra correspondente ao Estado
   scanf(" %c", &Estado2);
 
-  printf("Digite o Código da Carta (ex: A01): ");
+  printf("Digite o Código da Carta (ex: A01): ");//O jogador ira digitar o código da carta
   scanf("%3s", Codigo_da_carta2);
 
-  printf("Digite o Nome da Cidade: ");
+  printf("Digite o Nome da Cidade: ");//O jogador ira digitar o nome da cidade presente na carta
   scanf("%s", Nome_da_cidade2);
 
-  printf("Digite a População: ");
+  printf("Digite a População: ");//O jogador ira digitar a populaçao da cidade
   scanf("%d", &Populacao2);
 
-  printf("Digite a Área da Cidade (km²): ");
+  printf("Digite a Área da Cidade (km²): ");//O jogador ira digitar a área da cidade
   scanf("%f", &Area2);
 
-  printf("Digite o PIB (bilhões de reais): ");
+  printf("Digite o PIB (bilhões de reais): ");//O jogador ira digitar o PIB da cidade
   scanf("%f", &PIB2);
 
-  printf("Digite o N° de Pontos Turísticos: ");
+  printf("Digite o N° de Pontos Turísticos: ");//O jogador ira digitar o numero de pontos turísticos da cidade
   scanf("%d", &Pontos_Turisticos2);
 
   printf("\n\n");
 
   // Cálculos após a leitura dos dados
-  PIB_Per_Capita2 = (float) PIB2 / Populacao2;
+  PIB_Per_Capita2 = (float) (PIB2 * 1000000000) / Populacao2;//Será convertido bilhões para unidades
   Densidade_Populacional2 = (float) Populacao2 / Area2;
 
   // Exibição das cartas cadastradas
